83b1318faed5aa6138a075efcbc62eb7a1259e93
dipalipambhar158 <dipalipambhar508@gmail.com>
2024-01-17 10:54:20 +0530

    stu_result_react

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,50 @@
-import { useState , useEffect } from "react";
-import { Container , Row , Col } from "react-bootstrap";
+import "./App.css";
+import { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 
 function Main(){
 
-    const initialValues = {
-        rno:"",
+  const initialvalues = {
+    rno:"",   
+    name:"",
+    s1:"",
+    s2:"",
+    s3:"",
+    s4:"",
+    s5:"",
+    total:"",
+    per:""
+
+    };
+
+  const[data,setData] = useState(initialvalues);       
+  const[mydata,myalldata]=useState([]); 
+  const[edit,editdata]=useState([]);
+  const[isedit,issetedit]=useState(false); 
+  const[editid,seteditid]=useState(-1);
+  const[search,setSearch]=useState('')
+    
+
+  const handleChange = (e) =>{
+    setData({...data,[e.target.name]:e.target.value});
+}
+
+const btnhandler = () => {
+data.total= parseInt(data.s1) + parseInt(data.s2) + parseInt(data.s3) + parseInt(data.s4) + parseInt(data.s5);
+data.per = data.total/5;
+
+console.log(data);
+
+if(isedit)
+{
+ let edata = [...mydata]
+ edata[editid] = data;
+ myalldata(edata)
+
+ setData(
+    {
+        rno:"",   
         name:"",
         s1:"",
         s2:"",
@@ -13,184 +53,171 @@ function Main(){
         s5:"",
         total:"",
         per:""
-    }
-    const [data,setdata] = useState(initialValues);
-    const [mydata,myalldata] = useState([]);
-    const[edit,editdata] = useState([]);
-    const[isedit,issetedit] = useState();
-    const[editid,seteditid] = useState(-1);
-    const[search,setsearch] = useState('');
-
-    const handleChange = (e) =>{
-        setdata({...data,[e.target.name]:e.target.value});
-    }
-
-    const btnhandler = () =>{
-        data.total = parseInt(data.s1) + parseInt(data.s2) + parseInt(data.s3) + parseInt(data.s4) + parseInt(data.s5);
-        data.per = data.total/5;
-        console.log(data);
-
-        if(isedit){
-            let edata = [...mydata];
-            edata[editid] = data
-            myalldata(edata)
-            setdata({
-                    rno:"",
-                    name:"",
-                    s1:"",
-                    s2:"",
-                    s3:"",
-                    s4:"",
-                    s5:"",
-                    total:"",
-                    per:""
-            })
-        }
-        else{
-            myalldata(mydata =>[...mydata,data]);
-        }
-
-    }
-    const deletehandler = (k) =>{
-        console.log(k);
-        const newPeople = mydata.filter((t,i) => i !==k);
-        myalldata(newPeople);
-    }
-    const edithandler = (k) =>{
-        seteditid(k);
-        issetedit(true)
-        const newPeople = mydata[k];
-        console.log(newPeople);
-        setdata(newPeople);
-    }
-
-    useEffect(()=>{
-        console.log(mydata);
-        console.log(edit)
-    },[mydata])
-    return(
-        <>
-            <h3 className="text-center">Student Result</h3>
-            <div className="container ">
-                <div className="row justify-content-center mt-5">
-                    <div className="col-lg-8">
-                        <div className="row">
-                            <div className="col-lg-2">
-                                Roll NO
-                            </div>
-                            <div className="col-lg-8">
-                                <input type="text" className="form-control" name="rno" value={data.rno} onChange={handleChange} placeholder="Enter roll no"></input>
-                            </div>
-                        </div>
-                        <div className="row mt-3">
-                            <div className="col-lg-2">
-                                Name
-                            </div>
-                            <div className="col-lg-8">
-                                <input type="text" className="form-control" name="name" value={data.name} onChange={handleChange} placeholder="Enter name"></input>
-                            </div>
-                        </div>
-                        <div className="row mt-3">
-                            <div className="col-lg-2">
-                                Python
-                            </div>
-                            <div className="col-lg-8">
-                                <input type="text" className="form-control" name="s1" value={data.s1} onChange={handleChange}></input>
-                            </div>
-                        </div>
-                        <div className="row mt-3">
-                            <div className="col-lg-2">
-                                Rubby
-                            </div>
-                            <div className="col-lg-8">
-                                <input type="text" className="form-control" name="s2" value={data.s2} onChange={handleChange}></input>
-                            </div>
-                        </div>
-                        <div className="row mt-3">
-                            <div className="col-lg-2">
-                                Php
-                            </div>
-                            <div className="col-lg-8">
-                                <input type="text" className="form-control" name="s3" value={data.s3} onChange={handleChange}></input>
-                            </div>
-                        </div>
-                        <div className="row mt-3">
-                            <div className="col-lg-2">
-                                React
-                            </div>
-                            <div className="col-lg-8">
-                                <input type="text" className="form-control" name="s4" value={data.s4} onChange={handleChange}></input>
-                            </div>
-                        </div>
-                        <div className="row mt-3">
-                            <div className="col-lg-2">
-                                Dart
-                            </div>
-                            <div className="col-lg-8">
-                                <input type="text" className="form-control" name="s5" value={data.s5} onChange={handleChange}></input>
-                            </div>
-                        </div>
-                       <div className="text-center mt-3"><button className="btn btn-outline-success" onClick={btnhandler} >Submit</button></div>
-                    </div>
-                </div>
-            </div>
-
-
-
-            <Container>
-                <Row className="text-center mt-4">
-                    <Col lg={12}>
-                        <input type="search" placeholder="Search" onChange={(e) => setsearch(e.target.value)}></input>
-                    </Col>
-                </Row>
-
-                <table border={2}>
-                    <tr>
-                        <th>Rno</th>
-                        <th>Name</th>
-                        <th>Sub1</th>
-                        <th>Sub2</th>
-                        <th>Sub3</th>
-                        <th>Sub4</th>
-                        <th>Sub5</th>
-                        <th>Total</th>
-                        <th>Per</th>
-                        <th>Action</th>
-                    </tr>
-                    {
-
-                        mydata.filter((el) =>{
-                            if(search){
-                                return el.name.includes(search)
-                            }
-                            else{
-                                return el
-                            }
-                        }).map((item,k)=>{
-                            return(
-                                <tr>
-                                    <th>{item.rno}</th>
-                                    <th>{item.name}</th>
-                                    <th>{item.s1}</th>
-                                    <th>{item.s2}</th>
-                                    <th>{item.s3}</th>
-                                    <th>{item.s4}</th>
-                                    <th>{item.s5}</th>
-                                    <th>{item.total}</th>
-                                    <th>{item.per}</th>
-                                    <th>
-                                        <button onClick={()=>(deletehandler(k))}>Delete</button>
-                                    </th>
-                                    <th>
-                                        <button onClick={()=>(edithandler(k))}>Edit</button>
-                                    </th>
-                                </tr>
-                            )
-                        })
-                    }
-                </table>
-            </Container>
-        </>
-    )
+     }
+ )
 }
+else{
+ myalldata(mydata => [...mydata,data]);
+
+ setData(
+    {
+        rno:"",   
+        name:"",
+        s1:"",
+        s2:"",
+        s3:"",
+        s4:"",
+        s5:"",
+        total:"",
+        per:""
+     }
+ )
+}
+
+
+}
+const deletehandler = (k) =>{
+   console.log(k);
+   const newPeople = mydata.filter((t,i) => i !== k );
+   myalldata(newPeople);
+}
+const edithandler = (k) => {
+   
+seteditid(k);
+issetedit(true)
+const newPeople = mydata[k];
+console.log(newPeople);
+setData(newPeople);
+}
+
+
+useEffect(() => {
+console.log(mydata);
+console.log(edit);
+},[mydata,edit])
+
+
+    return(
+        <Container>
+
+        <div className='row'>
+            <div className='col-lg-12 mb-5 mt-5 text-center'><h2>STUDENT FORM</h2></div>
+        </div>
+        <div className= 'row mb-3'>
+            <div className='col-lg-2'>
+                <p className='fs-5'>Roll No:</p>
+            </div>
+            <div className='col-lg-8'>
+                <input type="text" value={data.rno} name="rno" onChange={handleChange} className="form-control"  placeholder="Enter Your ROll No" />
+            </div>
+        </div>
+        <div className='row mb-3'>
+            <div className='col-lg-2'>
+                <p className='fs-5'>Name:</p>
+            </div>
+            <div className='col-lg-8'>
+                <input type="text" value={data.name} name="name" onChange={handleChange} className="form-control"  placeholder="Enter Your Name" />
+            </div>
+        </div>
+        <div className='row mb-3'>
+            <div className='col-lg-2'>
+                <p className='fs-5'>React Js:</p>
+            </div>
+            <div className='col-lg-8'>
+                <input type="text" value={data.s1} name="s1" onChange={handleChange} className="form-control"  placeholder="Enter Your Marks" />
+            </div>
+        </div>
+        <div className='row mb-3'>
+            <div className='col-lg-2'>
+                <p className='fs-5'>Node Js:</p>
+            </div>
+            <div className='col-lg-8'>
+                <input type="text" value={data.s2} name="s2" onChange={handleChange} className="form-control"  placeholder="Enter Your Marks" />
+            </div>
+        </div>
+        <div className='row mb-3'>
+            <div className='col-lg-2'>
+                <p className='fs-5'>Python:</p>
+            </div>
+            <div className='col-lg-8'>
+                <input type="text" value={data.s3} name="s3" onChange={handleChange} className="form-control"  placeholder="Enter Your Marks" />
+            </div>
+        </div>
+        <div className='row mb-3'>
+            <div className='col-lg-2'>
+                <p className='fs-5'>Rubby:</p>
+            </div>
+            <div className='col-lg-8'>
+                <input type="text" value={data.s4} name="s4" onChange={handleChange} className="form-control"  placeholder="Enter Your Marks" />
+            </div>
+        </div>
+        <div className='row mb-3'>
+            <div className='col-lg-2'>
+                <p className='fs-5'>Angular:</p>
+            </div>
+            <div className='col-lg-8'>
+                <input type="text" value={data.s5} name="s5" onChange={handleChange} className="form-control"  placeholder="Enter Your Marks" />
+            </div>
+        </div>
+      
+      <div className="col-12 mb-4">
+        <input type="submit" onClick={btnhandler} className="btn btn-success" />
+      </div>
+      <div className='row'>
+        <div className='col-lg-12 mb-5'>
+            <input type='search' value={search} onChange={(e) => setSearch(e.target.value)}></input>
+        </div>
+      </div>
+
+      <table className="row table" >
+        <thead className="col-lg-12">
+            <tr className="row table-dark p-3 fs-5">
+                <th className="col-lg-1">RNo</th>
+                <th className="col-lg-2">Name</th>
+                <th className="col-lg-1">React</th>
+                <th className="col-lg-1">Node</th>
+                <th className="col-lg-1">Python</th>
+                <th className="col-lg-1">Rubby</th>
+                <th className="col-lg-1">Angular</th>
+                <th className="col-lg-1">Total</th>
+                <th className="col-lg-1">Per</th>
+                <th className="col-lg-2">Action</th>
+            </tr>
+        </thead>
+
+        {
+            mydata.filter((el) => {
+                if(search){
+                    return el.name.includes(search)
+                }
+                else{
+                    return el
+                }
+            }).map ((item,k)=>{
+                return(
+                    <tr className="row fs-5">
+                        <th className="col-lg-1">{item.rno} </th>
+                        <th className="col-lg-2">{item.name} </th>
+                        <th className="col-lg-1">{item.s1} </th>
+                        <th className="col-lg-1">{item.s2} </th>
+                        <th className="col-lg-1">{item.s3} </th>
+                        <th className="col-lg-1">{item.s4} </th>
+                        <th className="col-lg-1">{item.s5} </th>
+                        <th className="col-lg-1">{item.total} </th>
+                        <th className="col-lg-1">{item.per} </th>
+                        <th className="col-lg-2"><button className="btn btn-outline-danger px-3" onClick={()=> (deletehandler(k))}>Delete</button>
+                        <button className="ms-3 btn btn-outline-success px-3" onClick={()=> (edithandler(k))}>Edit</button></th>
+                    </tr>
+                )
+            })
+        
+        }
+      </table>
+
+        </Container>
+
+    )
+
+}
+
 export default Main
